add566074641fc53d33d9142497313c5bd52bc78
luqinhas <50590804+luqinhas@users.noreply.github.com>
2019-05-13 14:25:48 -0300

    com ajuda da pedrita

---
+++ 1aula.py
@@ -0,0 +1,30 @@
+# Meu primeiro programa
+from random import randint # Importa a função de gerar números aleatórios
+
+print("Jogo dos Dados: ")
+
+dado1 = randint(1, 6)
+print("Dado 1  - ", dado1)
+
+dado2 = randint(1, 6) # gera um número entre 1 e 6 aleatoriamente
+print("Dado 2  - ", dado2)
+
+dado3 = randint(1, 6)
+print("Dado 3  - ", dado3)
+
+dado4 = randint(1, 6)
+print("Dado 4  - ", dado4)
+
+Jogador1 = dado1 + dado2
+Jogador2 = dado3 + dado4
+
+print('Jogador1  -', Jogador1)
+print('Jogador2  - ', Jogador2)
+
+if Jogador1 > Jogador2:
+    print('Jogador 1 venceu!')
+else: 
+    if Jogador2 > Jogador1:
+        print("Jogador 2 Venceu!")
+    else:
+        print("Empate")
